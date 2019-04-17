8e4f4f44e6efee14637eafe1c24eb30d0f136961
梁凯 <2239770935@qq.com>
2019-04-17 14:53:39 +0800

    。。。

--- test-yssp/src/main/java/com/test/pojo/UserResult.java
+++ test-yssp/src/main/java/com/test/pojo/UserResult.java
@@ -1,16 +1,24 @@
 package com.test.pojo;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserResult {
 
     private String temporaryBill;
     private String sessionKey;
 
+    public String getTemporaryBill() {
+        return temporaryBill;
+    }
 
+    public void setTemporaryBill(String temporaryBill) {
+        this.temporaryBill = temporaryBill;
+    }
+
+    public String getSessionKey() {
+        return sessionKey;
+    }
+
+    public void setSessionKey(String sessionKey) {
+        this.sessionKey = sessionKey;
+    }
 }
